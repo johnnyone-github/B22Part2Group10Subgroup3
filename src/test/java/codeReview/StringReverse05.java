d854219d5572dab291149e5e46d80378acf93b96
John Clancy <johnpclancy@hotmail.com>
2021-05-26 09:59:21 -0400

    Added code

--- src/test/java/codeReview/StringReverse05.java
+++ src/test/java/codeReview/StringReverse05.java
@@ -9,5 +9,15 @@ public class StringReverse05 {
     public static void main(String[] args) {
 
     }
+public static String reverseString(String str) {
+        String str2 = "";
+    for (int i = str.length()-1; i < 0; i--) {
+        str2 += "" + str.charAt(i);
+    }
+    return str2;
+}
+
+
+
 
 }
