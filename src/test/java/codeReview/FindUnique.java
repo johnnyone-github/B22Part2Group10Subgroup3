b85e0e0fb99f495f23ffabc77e4a8b795c9f32e1
B22 Ant <nknwn1882@gmail.com>
2021-05-27 13:34:19 -0500

    Added solution 2

--- src/test/java/codeReview/FindUnique.java
+++ src/test/java/codeReview/FindUnique.java
@@ -1,5 +1,8 @@
 package codeReview;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * [String -- Find the unique]
  * Write a return  method that can find the unique characters from the String
@@ -9,15 +12,21 @@ package codeReview;
 public class FindUnique {
     public static void main(String[] args) {
         String str = "AAABBBCCCDEF";
+        System.out.println(str);
 
         System.out.println(findUnique(str));
+        System.out.println(Unique(str));
     }
 
     public static String findUnique(String str) {
+
         String uniq = "";
+
         for (int i = 0; i < str.length(); i++) {
+
             int count = 0;
             for (int j = 0; j < str.length(); j++) {
+
                 if (str.charAt(i) == str.charAt(j)) {
                     count++;
                 }
@@ -27,6 +36,20 @@ public class FindUnique {
             }
         }
         return uniq;
+    }
+
+    public static String Unique(String str) {
+
+        String result ="";
+
+        for(String each : str.split(""))
+
+            result +=(  (Collections.frequency(Arrays.asList(str.split("")), each)) ==1 ) ? each : "";
+
+
+
+        return result;
+
     }
 
 }
