e58e7c2d3d924c6e29c58d9dc2bdf8cc2329aaaa
B22 Ant <nknwn1882@gmail.com>
2021-06-11 11:57:46 -0500

    Added Password Validation and ArraysSortAscending and ArraySortDescending

--- src/test/java/codeReview/Test02.java
+++ src/test/java/codeReview/Test02.java
@@ -1,7 +1,14 @@
 package codeReview;
 
+import org.testng.annotations.Test;
+
 public class Test02 {
 
     //started project JL
 
+
+    @Test
+    public void testName() {
+        System.out.println("works");
+    }
 }
