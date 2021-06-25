c72a8ee751f5a125bda1a935bb92040c2e61fdd9
B22 Ant <nknwn1882@gmail.com>
2021-06-25 12:13:26 -0500

    Added FinRa task

--- src/test/java/codeReview/ArraySortDescending.java
+++ src/test/java/codeReview/ArraySortDescending.java
@@ -1,11 +1,11 @@
 package codeReview;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
  * Array -- Sort Ascending
- * Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
+ * Write a return method that can sort an int array in
+ * Ascending order without using the sort method of the Arrays class
  * Ex:
  * int[] arr = {10, 9, 8, 7};
  * arr = Sort(arr);  ==>{ 7, 8, 9, 10};
@@ -13,12 +13,18 @@ import java.util.Arrays;
 
 public class ArraySortDescending {
     public static void main(String[] args) {
-        int[] arr = {78, 34, 1, 3, 90, 34, -1, -4, 6, 55, 20, -65};
+
+        int[] arr = {10, 9, 8, 7};
+
         //sorting logic
         for (int i = 0; i < arr.length; i++) {
+
             for (int j = i + 1; j < arr.length; j++) {
+
                 int temp = 0;
-                if (arr[i] < arr[j]) {
+
+                if (arr[i] > arr[j]) {
+
                     temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
