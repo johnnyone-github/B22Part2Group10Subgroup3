45ad334cc68725bdaecc7a320008b275d5a5e2f6
John Clancy <johnpclancy@hotmail.com>
2021-07-02 11:15:12 -0400

    print consecutive numbers is complete

--- src/test/java/codeReview/Numbers_PrintConsecutiveNumbers.java
+++ src/test/java/codeReview/Numbers_PrintConsecutiveNumbers.java
@@ -1,6 +1,28 @@
 package codeReview;
 
 public class Numbers_PrintConsecutiveNumbers {
+    public static void main(String[] args) {
+        printConsecutiveNumbers(24);
+    }
 
+    public static void printConsecutiveNumbers(int n) {
+        for (int i = 1; i <= n; i++) {
+            if(i % 2 != 0 && i % 3 != 0 && i % 5 != 0){
+                System.out.println(i);
+                continue;
+            }
+            if (i % 2 == 0) {
+                System.out.print("Codility");
+            }
+            if (i % 3 == 0) {
+                System.out.print("Test");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Coders");
+            }
+            System.out.println("");
+        }
+
+    }
 }
 
